d5668028ac21d266ae0303f0a04170df40bbabf0
Shelden <shelden@berkeley.edu>
2020-12-27 11:26:59 -0800

    added poin.java

---
+++ src/Core/Point.java
@@ -0,0 +1,21 @@
+package byow.Core;
+
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    private static final long serialVersionUID = 652965098267757690L;
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
