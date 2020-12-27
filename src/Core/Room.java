f5b22cf777cb759ca18088436f6def84541b1a1e
Shelden <shelden@berkeley.edu>
2020-12-27 11:28:03 -0800

    upload project

---
+++ src/Core/Room.java
@@ -0,0 +1,150 @@
+package byow.Core;
+
+import byow.TileEngine.TERenderer;
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import java.io.Serializable;
+import java.util.*;
+
+
+public class Room implements Serializable {
+    private static final long serialVersionUID = 652968509826775790L;
+    private static final int MAXROOM = 10;
+    // length and width of the room
+    private int w;
+    private int l;
+    // coordinate of the room
+    private int xLeft;
+    private int xRight;
+    private int yTop;
+    private int yBottom;
+
+
+    public Room(int x, int y, int l, int w) {
+        this.xLeft = x;
+        this.xRight = x + w;
+        this.yTop = y;
+        this.yBottom = y + l;
+        this.w = w;
+        this.l = l;
+    }
+    public Room(Random r) {
+        this.w = r.nextInt(10) +5;
+        this.l = r.nextInt(5) + 3;
+        this.xLeft = r.nextInt(Engine.WIDTH - w - 2) + 1;
+        this.xRight = xLeft + w;
+        this.yTop = r.nextInt(Engine.HEIGHT - l - 4) + 1;
+        this.yBottom = yTop + l;
+
+    }
+
+
+    public int getxLeft() {
+        return xLeft;
+    }
+
+    public int getxRight() {
+        return xRight;
+    }
+
+    public int getyTop() {
+        return yTop;
+    }
+
+    public int getyBottom() {
+        return yBottom;
+    }
+
+    public int getWidth() {
+        return w;
+    }
+
+    public int getLength() {
+        return l;
+    }
+
+
+
+
+    // generate a new room and draw it
+    public static Room drawRoom(Random r, TETile[][] world, ArrayList<Room> existingRooms) {
+        Room room = new Room(r);
+        while (room.overlap(existingRooms)){
+            room = new Room(r);
+        }
+
+        for (int x = room.xLeft; x < room.xRight; x++) {
+            for (int y = room.yTop; y < room.yBottom; y++) {
+                world[x][y] = Tileset.FLOOR;
+            }
+        }
+
+
+        return room;
+    }
+
+
+
+
+
+    /*
+        (x0,y1) ------------ (x1,y1)
+           |                    |
+           |                    |
+           |                    |
+           |                    |
+        (x0,y0) ------------ (x1,y0)
+
+        x is column, y is row
+     */
+
+    public boolean overlap(ArrayList<Room> existingRooms) {
+        if (existingRooms.size() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < existingRooms.size(); i++) {
+        /*
+            Room curr = existingRooms.get(i);
+            // Left x
+            int leftX = Math.max(this.getxLeft(), curr.getxLeft());
+            // Right x
+            int rightX = Math.min(this.getxRight(), curr.getxRight());
+            // Bottom y
+            int botY = Math.max(this.getyBottom(), curr.getyBottom());
+            // TopY
+            int topY = Math.min(this.getyTop(), curr.getyTop());
+
+            if (rightX >= leftX && topY >= botY)
+                return true;
+            }
+        return false;
+
+         */
+            Room curr = existingRooms.get(i);
+            if ((this.getxRight() < curr.getxLeft() || this.getxLeft() > curr.getxRight()) ||
+                    (this.getyBottom() > curr.getyTop() || this.getyTop() < curr.getyBottom())) {
+                continue;
+            }
+
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+    public static ArrayList<Room> roomGenerator(Random r, TETile[][] world) {
+        int numOfRooms = r.nextInt(MAXROOM - 2) + 2;
+        ArrayList<Room> rooms = new ArrayList<Room>();
+
+        for (int i = 0; i < numOfRooms; i++) {
+            Room room = drawRoom(r, world, rooms);
+            rooms.add(room);
+        }
+        return rooms;
+    }
+
+}
---
+++ src/Core/WorldObjects.java
@@ -0,0 +1,55 @@
+package byow.Core;
+
+import byow.TileEngine.TETile;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WorldObjects implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private ArrayList<Room> rooms;
+    private ArrayList<Point> doors = new ArrayList<>();
+    private Point avatar;
+    private TETile[][] world;
+    private Random r;
+    private char avatarDir;
+
+    public WorldObjects(TETile[][] world) {
+        this.world = world;
+    }
+    public TETile[][] getWorld() {
+        return world;
+    }
+
+    public ArrayList<Point> getDoors() {
+        return doors;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+    public void setAvatar(Point point, char dir) {
+        avatar = point;
+        avatarDir = dir;
+    }
+
+    public char getAvatarDir() {
+        return avatarDir;
+    }
+
+    public Random getR() {
+        return r;
+    }
+
+    public void setR(Random r) {
+        this.r = r;
+    }
+
+    public Point getAvatar() {
+        return avatar;
+    }
+}
